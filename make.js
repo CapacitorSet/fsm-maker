0822269a81c16082cf1f5fc60ee54e4a1bbf101d
Capacitor Set <CapacitorSet@users.noreply.github.com>
2016-01-19 23:10:12 +0100

    Rimosso 'esponente' dal formato di ingresso

--- make.js
+++ make.js
@@ -162,12 +162,12 @@ stringa.BUS_ENABLED = Number(dati.io.bus.length != 0);
 stringa.DEBOUNCE_ENABLED = Number(dati.antirimbalzo.porte.length != 0);
 
 i = 1;
+
 stringa.INPUT_COUNTS = dati.io.input.map(x => 127).toString();
 stringa.INGRESSI_DEBOUNCED = dati.io.input
 	.map(x => dati.antirimbalzo.porte.indexOf(x) != -1)
 	.map(Number)
 	.toString();
-stringa.COSTANTE_PER_RC = dati.antirimbalzo.esponente;
 stringa.SOGLIA_BASSA = dati.antirimbalzo.schmitt.basso; // 0..255
 stringa.SOGLIA_ALTA = dati.antirimbalzo.schmitt.alto; // 0..255
 stringa.INTERVALLO = dati.antirimbalzo.schmitt.intervallo || 1; // Defaulta a 1
